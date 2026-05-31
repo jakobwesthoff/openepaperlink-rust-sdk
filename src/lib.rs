9381e5a13a74c835d2471db97621dffb7ac986e9
Jakob Westhoff <jakob@westhoffswelt.de>
2026-05-31 18:55:10 +0200

    Add crate-level doc examples for docs.rs landing page

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,58 @@
 #![deny(missing_docs)]
 
 //! Rust SDK for communicating with
-//! [OpenEPaperLink](https://github.com/OpenEPaperLink/OpenEPaperLink) access
-//! points.
+//! [OpenEPaperLink](https://github.com/OpenEPaperLink/OpenEPaperLink) (OEPL)
+//! access points.
 //!
 //! Provides typed HTTP and WebSocket clients for managing e-paper tags,
 //! configuring the AP, uploading images, and subscribing to real-time events.
+//!
+//! # Listing tags and uploading an image
+//!
+//! ```no_run
+//! use openepaperlink_sdk::{Client, Mac, UploadImageOptions};
+//!
+//! # async fn run() -> Result<(), openepaperlink_sdk::Error> {
+//! let client = Client::builder("http://192.168.1.100").build()?;
+//!
+//! // List all tags the AP knows about
+//! for tag in client.get_tags().await? {
+//!     println!("{} hw=0x{:02X} battery={:?}", tag.mac, tag.hw_type, tag.battery);
+//! }
+//!
+//! // Push a JPEG image to a specific tag
+//! let mac: Mac = "00007E231842B297".parse()?;
+//! let jpeg = std::fs::read("label.jpg").expect("read image");
+//! client.upload_image(&mac, jpeg, &UploadImageOptions::default()).await?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! # Streaming real-time events
+//!
+//! ```no_run
+//! use openepaperlink_sdk::{Client, StreamExt, WsMessage};
+//!
+//! # async fn run() -> Result<(), openepaperlink_sdk::Error> {
+//! let client = Client::builder("http://192.168.1.100").build()?;
+//! let mut stream = client.connect_ws().await?;
+//!
+//! while let Some(Ok(msg)) = stream.next().await {
+//!     match msg {
+//!         WsMessage::TagUpdate(tags) => {
+//!             for tag in &tags {
+//!                 println!("tag {} checked in", tag.mac);
+//!             }
+//!         }
+//!         WsMessage::SystemInfo(sys) => {
+//!             println!("heap={} tags={}", sys.heap, sys.recordcount);
+//!         }
+//!         _ => {}
+//!     }
+//! }
+//! # Ok(())
+//! # }
+//! ```
 
 mod client;
 mod config;
